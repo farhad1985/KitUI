ad99647b3a5dee84a7bac0fc573921c3b2a0a13e
Sherlock <farhad.public@gmail.com>
2021-06-22 14:39:58 +0430

    Initial Commit

---
+++ KitUI/KitUI.h
@@ -0,0 +1,18 @@
+//
+//  KitUI.h
+//  KitUI
+//
+//  Created by Farhad on 6/22/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KitUI.
+FOUNDATION_EXPORT double KitUIVersionNumber;
+
+//! Project version string for KitUI.
+FOUNDATION_EXPORT const unsigned char KitUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KitUI/PublicHeader.h>
+
+
